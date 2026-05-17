a67fccf6721bf9a0c47ec73a33d403d0249cf633
Veronika Malinovska <malinowskawell@gmail.com>
2026-05-17 18:46:12 +0200

    solved some problems

---
+++ easy/two-sum.cpp
@@ -0,0 +1,30 @@
+//
+// Created by nika mally on 17/05/2026.
+//
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> helper;
+        for (int i=0; i<nums.size(); i++) {
+            int tmp = target - nums.at(i);
+            if (helper.contains(tmp)) {
+                return {helper[tmp], i};
+            }
+            helper[nums.at(i)] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    Solution s;
+    vector<int> ans = s.twoSum(nums, 9);
+    for (auto i : ans)
+        cout << i << ' ';
+    return 0;
+}
